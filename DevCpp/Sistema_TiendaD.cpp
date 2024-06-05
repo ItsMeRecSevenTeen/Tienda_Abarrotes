4ae5300f1c5e59b5e2bf7927b1a153b6102faafc
ItsMeRecSevenTeen <rickagent478554@gmail.com>
2024-06-05 05:53:46 -0600

    Se está desarrollando el sistema de escritura de ficheros al añadir un articulo al inventario, Al igual que se definió el constructor de la Clase padre 'Producto' y la clase hija 'Perecederos'

--- DevCpp/Sistema_TiendaD.cpp
+++ DevCpp/Sistema_TiendaD.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
 #include <string>
 #include <windows.h>
+#include <fstream>  // Para ofstream
+#include <iostream> // Para cout
 #include "PersonaD.h"
 #include "ProductoD.h"
 
 #define color SetConsoleTextAttribute
 
 using namespace std;
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+Producto::Producto(){
+	cout << "Ingrese el nombre del producto: ";color(hConsole, 6);cout << "\tSin espacios, 20 caracteres\n";color(hConsole, 208);cin>>nombre;
+	color(hConsole, 13);cout << "Ingrese la fecha de caducidad: ";color(hConsole, 6);cout << "\t'dd/mm/aaaa'\n";color(hConsole, 208);cin>>fechadecad;
+	color(hConsole, 13);cout << "Cuanto contenido neto tiene el producto?: ";color(hConsole, 6);cout << "\t(grs, kgs, mls, lts, gls)\n";color(hConsole, 208);cin>>cantidad;
+	color(hConsole, 13);cout << "Ingrese la cantidad de productos?: ";color(hConsole, 6);cout << "\tUn numero\n";color(hConsole, 208);cin>>unidades;
+	color(hConsole, 13);cout << "Ingrese el precio unitario del producto?: ";color(hConsole, 6);cout << "\tPrecio con 2 decimas de centavo\n";color(hConsole, 208);cin>>precio;
+}
+Perecederos::Perecederos(){
+    color(hConsole, 13);cout << "Cual es la temperatura de almacenamiento del producto perecedero?: ";color(hConsole, 6);cout << "\tUn numero, °C\n";cin >> temperaturaalmacencelcius;
+    color(hConsole, 13);cout << "Tipo de almacenamiento que requiere el producto?: ";color(hConsole, 6);cout << "\t(Ambiente,Refrigeracion, Congelacion)\n";cin>> tipoconservacion;
+}
+void Perecederos::DarAlta(Perecederos miPerecedero){
+    ofstream fich("Articulos.txt");
+  				if (!fich){
+				    cout << "Error al abrir Articulos.txt\n";
+    				exit(EXIT_FAILURE);
+				}
+				fich << miPerecedero.codigo << "," << miPerecedero.nombre << "," << miPerecedero.cantidad << "," << miPerecedero.precio << "," << miPerecedero.fechadecad << "," << miPerecedero.unidades << "\n";
+}
 
 void MenuPrincipal();
 void menuCliente(){
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     int x;
     do{
         color(hConsole, 1);cout << "Que desea realizar?\t";
@@ -22,8 +43,9 @@ void menuCliente(){
             cout << "Saliendo del sistema";
             exit(0);
             break;
-        case 1:
-            break;
+        case 1:{
+			break;
+		}
         case 2:
             break;
         case 3:
@@ -40,7 +62,6 @@ void menuCliente(){
 }
 void menuAdmin();
 void MenuPrincipal(){
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	int x;
 	do{
  		color(hConsole, 2);cout << "Bienvenido a la tienda de abarrotes... Que desea hacer? \t";
@@ -67,7 +88,6 @@ void MenuPrincipal(){
 	}while(x != 0);
 }
 void menuAdmin(){
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
  	color(hConsole, 13);cout << "Entrando como administrador...\n";
 
 	string usuario = "admin", contrasena = "1234", contrasenaUser = "0";
@@ -91,7 +111,7 @@ void menuAdmin(){
 		}
 	}while(contrasena != contrasenaUser);
 	do{
-		/*color(hConsole, 5);*/cout << "Bienvenido... Que desea hacer?\t";
+		color(hConsole, 13);cout << "Bienvenido... Que desea hacer?\t";
 		color(hConsole, 208);cout << "Administrador";
 		color(hConsole, 13);cout << "\n1.-Anadir item a inventario\n2.-Eliminar item de inventario\n3.-Modificar item de inventario\n4.-Mostrar items del inventario\n0.-Cerrar sesion y volver al menu principal\n";cin >> x;
 		switch(x){
@@ -99,7 +119,21 @@ void menuAdmin(){
 				MenuPrincipal();
 				break;
 			case 1:
-				break;
+                cout << "Que tipo de articulo anadira al inventario?\n1.-Perecedero\n2.-No Perecedero\n";cin >> x;
+			    switch(x){
+                    case 1:{
+                        Perecederos miPerecedero;
+                        miPerecedero.DarAlta(miPerecedero);
+                        color(hConsole, 6);cout << "Producto anadido al inventario\n";
+                        break;
+                    }
+                    case 2:{
+                    }
+                    default:
+                    color(hConsole, 64);
+                    cout << "Opcion invalida, Intentelo de nuevo...\n";
+                    color(hConsole, 2);
+			    }
 			case 2:
 				break;
 			case 3:
